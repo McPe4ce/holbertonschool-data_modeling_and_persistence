a38222f0ae1aea69d93b162396435fe7bf9f8258
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-01 16:09:04 +0200

    fix: change data type from MONEY to REAL for price in books table

--- sql_crud/0-create_table.sql
+++ sql_crud/0-create_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE books (
     title TEXT NOT NULL,
     author TEXT NOT NULL,
     genre TEXT NOT NULL,
-    price MONEY NOT NULL,
+    price REAL NOT NULL,
     stock INTEGER,
     published_year INTEGER NOT NULL
 );

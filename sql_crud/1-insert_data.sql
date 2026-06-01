4181795460725677b7b2a8db560c629cf77d645d
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-01 16:45:51 +0200

    fix: use INSERT OR REPLACE for data insertion in books table

--- sql_crud/1-insert_data.sql
+++ sql_crud/1-insert_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO books (id, title, author, genre, price, stock, published_year) VALUES 
+INSERT OR REPLACE INTO books (id, title, author, genre, price, stock, published_year) VALUES 
 ('1', 'The Hobbit', 'J.R.R. Tolkien', 'Fantasy', '10.99', '5', '1937'),
 ('2', '1984', 'George Orwell', 'Dystopian', '8.99', '12', '1949'),
 ('3', 'Clean Code', 'Robert C. Martin', 'Tech', '32.50', '7', '2008'),

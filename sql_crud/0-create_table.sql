c4db4aa49f40d300d0e64aababb0e1a78e3210c3
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-01 16:06:41 +0200

    fix: change data type from INT to INTEGER for consistency in books table

--- sql_crud/0-create_table.sql
+++ sql_crud/0-create_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE books (
-    id INT PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     author TEXT NOT NULL,
     genre TEXT NOT NULL,
     price MONEY NOT NULL,
-    stock INT,
-    published_year INT NOT NULL
+    stock INTEGER,
+    published_year INTEGER NOT NULL
 );
 

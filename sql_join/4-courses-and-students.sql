6b665e6c96e4a077d1d025423b8206a613e1ab60
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-03 16:35:52 +0200

    feat: add query to retrieve course titles and student names with sorting

---
+++ sql_join/4-courses-and-students.sql
@@ -0,0 +1,5 @@
+SELECT courses.title, students.name
+FROM courses
+LEFT JOIN enrollments ON enrollments.course_id = courses.id
+LEFT JOIN students ON enrollments.student_id = students.id
+ORDER BY courses.title, students.name ASC

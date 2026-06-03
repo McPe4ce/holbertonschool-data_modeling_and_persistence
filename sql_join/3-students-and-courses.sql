9acc2872e3a7968b35b72a8489e7ec35201a61f4
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-03 16:27:57 +0200

    fix: update ORDER BY clause to include course titles for sorted results

--- sql_join/3-students-and-courses.sql
+++ sql_join/3-students-and-courses.sql
@@ -2,4 +2,4 @@ SELECT students.name, courses.title
 FROM enrollments
 INNER JOIN students ON enrollments.student_id = students.id
 INNER JOIN courses ON enrollments.course_id = courses.id
-ORDER BY students.name ASC
+ORDER BY students.name, courses.title ASC

3d4285514472fc38a291b3cf3e75932e9389e2fe
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-05 13:15:04 +0200

    fix: change JOIN type to LEFT JOIN for correct course-instructor association

--- sql_join/9-instructors-and-courses.sql
+++ sql_join/9-instructors-and-courses.sql
@@ -1,4 +1,4 @@
 SELECT instructors.name, courses.title
 FROM instructors
-RIGHT JOIN courses ON courses.id = instructors.id
+LEFT JOIN courses ON courses.id = instructors.id
 ORDER BY instructors.name ASC, courses.title ASC

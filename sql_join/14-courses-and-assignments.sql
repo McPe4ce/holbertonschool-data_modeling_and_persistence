dd5d57be44d8602001ab187a37ef0ed158222620
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-05 13:41:01 +0200

    fix: correct JOIN type to LEFT JOIN for accurate course-assignments association

--- sql_join/14-courses-and-assignments.sql
+++ sql_join/14-courses-and-assignments.sql
@@ -1,4 +1,4 @@
 SELECT courses.title, assignments.title
-FROM assignments
-RIGHT JOIN courses ON courses.id = assignments.course_id
+FROM courses
+LEFT JOIN assignments ON assignments.course_id = courses.id
 ORDER BY courses.title ASC, assignments.title ASC

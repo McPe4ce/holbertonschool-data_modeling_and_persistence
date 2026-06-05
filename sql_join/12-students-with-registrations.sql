6061598f08786c220b0850aa8c6511e485097007
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-05 13:26:22 +0200

    fix: update query to select student names and order results alphabetically

--- sql_join/12-students-with-registrations.sql
+++ sql_join/12-students-with-registrations.sql
@@ -1,6 +1,6 @@
-SELECT courses.title, COUNT(courses.id)
+SELECT students.name
 FROM students
 INNER JOIN registrations ON registrations.student_id = students.id
 INNER JOIN courses ON courses.id = registrations.course_id
 GROUP BY registrations.student_id
-ORDER BY COUNT(courses.id) DESC
+ORDER BY students.name ASC
